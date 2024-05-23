d5259d2a29070c4bdc230a571edb6f03b2956a25
r4bb1t <r4bb1t.github@gmail.com>
2024-05-23 13:15:13 +0900

    ladle: add input

--- src/stories/button.stories.tsx
+++ src/stories/button.stories.tsx
@@ -1,4 +1,4 @@
-import { Button } from "..";
+import Button from "@/components/Button";
 
 import type { Story } from "@ladle/react";
 
---
+++ src/stories/form.stories.tsx
@@ -0,0 +1,11 @@
+import Input from "@/components/Input";
+
+import type { Story } from "@ladle/react";
+
+export const TextInput: Story = () => (
+  <div className="flex flex-col gap-4">
+    {["xs", "sm", "md", "lg"].map((sz) => (
+      <Input type="text" label={sz} sz={sz as "xs" | "sm" | "md" | "lg"} />
+    ))}
+  </div>
+);
