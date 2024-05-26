267ecddaf79c5a64035a9836676c767adcdb3d5d
r4bb1t <r4bb1t.github@gmail.com>
2024-05-26 21:45:22 +0900

    feat: add export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export { default as Button } from "./components/Button";
 export { default as Spinner } from "./components/Spinner";
+export { default as Input } from "./components/Input";
 export { default as Layout } from "./components/Layout";
+
+export { useContextStore } from "./store/contextStore";
 
 export { config as tailwindPreset } from "../tailwind.config";
