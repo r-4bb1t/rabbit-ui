d5dcc81b0f948d4ae3c466bd4da1c7ba3b1846e4
r4bb1t <r4bb1t.github@gmail.com>
2024-05-31 19:46:19 +0900

    config: rollup setting

---
+++ rollup.config.ts
@@ -0,0 +1,48 @@
+import type { RollupOptions } from "rollup";
+import { dts } from "rollup-plugin-dts";
+
+import babel from "@rollup/plugin-babel";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+
+const sourceOptions: RollupOptions = {
+  input: "app/index.ts",
+  output: { dir: "dist", preserveModules: true },
+  external: [
+    "lucide-react",
+    "react",
+    "react/jsx-runtime",
+    "tailwindcss/defaultTheme",
+  ],
+  plugins: [
+    typescript({
+      tsconfig: "./app/tsconfig.json",
+    }),
+    babel({
+      babelHelpers: "bundled",
+      presets: [
+        "@babel/preset-env",
+        ["@babel/preset-react", { runtime: "automatic" }],
+        "@babel/preset-typescript",
+      ],
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    }),
+    terser(),
+  ],
+};
+
+const typeOptions: RollupOptions = {
+  input: "dist/types/index.d.ts",
+  output: {
+    file: "dist/index.d.ts",
+  },
+  plugins: [
+    dts({
+      tsconfig: "./app/tsconfig.json",
+    }),
+  ],
+};
+
+const rollupConfig = [sourceOptions, typeOptions];
+
+export default rollupConfig;
